c8388dae3b1b38d45d7f0a8fbefc3e8969a5d4b3
Marcos Paulo <marcos.dev1996@gmail.com>
2023-06-29 01:19:38 -0300

    search

--- js/main.js
+++ js/main.js
@@ -265,4 +265,19 @@ for (;i<l;i++) {
 	new Clock(clocks[i]);
 }
 
-  
+function myFunction() {
+    var input, filter, ol, li, a, i,  txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ol = document.getElementById("myUL");
+    li = ol.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h6")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
